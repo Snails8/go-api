4ba6435fa9438c3152070bdbc7a505c18d718182
Kaito Suzuki <snails8d@gmail.com>
2022-04-11 10:45:47 +0900

    reset: project 初期化

--- cmd/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/Snails8/go-api-sample
-
-go 1.16
-
-require golang.org/x/tools v0.1.8 // indirect
--- cmd/hello/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-// コンテナ確認時に使用。メッセージを出力
-//  docker-compose exec app go run hello/main.go
-func main() {
-	fmt.Println("Hello world")
-}
