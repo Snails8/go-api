9bfa7bc5f66c3c8585acb2c6373c111ec77acbd4
Kaito Suzuki <snails8d@gmail.com>
2022-04-21 09:40:06 +0900

    add: book のhandler 作成

---
+++ src/api/handler_blog.go
@@ -0,0 +1,72 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+// BookHandler : Book における Handler のインターフェース
+type BookHandler interface {
+	Index(http.ResponseWriter, *http.Request, httprouter.Params)
+}
+
+type bookHandler struct {
+	bookUseCase usecase.BookUseCase
+}
+
+// Book データに関する Handler を生成
+func NewBookHandler(bu usecase.BookUseCase) BookHandler {
+	return &bookHandler{
+		bookUseCase: bu,
+	}
+}
+
+// BookIndex : GET /books を返却
+func (bh bookHandler) Index(w http.ResponseWriter, r *http.Request, pr httprouter.Params) {
+	// request : 本 API のリクエストパラメータ
+	type request struct {
+		Begin uint `query:"begin"`
+		Limit uint `query:"limit"`
+	}
+
+	// bookField : response 内で使用する Book を表す構造体
+	//  -> ドメインモデルの Book に HTTP の関心事である JSON タグを付与したくないために Handler 層で用意
+	//     簡略化のために JSON タグを付与したドメインモデルを流用するプロジェクトもしばしば見かける
+	type bookField struct {
+		Id       int64     `json:"id"`
+		Title    string    `json:"title"`
+		Author   string    `json:"author"`
+		IssuedAt time.Time `json:"issued_at"`
+	}
+
+	// response : 本 API のレスポンス
+	type response struct {
+		Books []bookField `json:"books"`
+	}
+
+	ctx := r.Context()
+	// ユースケースの呼出
+	books, err := bh.bookUseCase.GetAll(ctx)
+	if err != nil {
+		// TODO: エラーハンドリングをきちんとする
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// 取得したドメインモデルを response に変換
+	res := new(response)
+	for _, book := range books {
+		var bf bookField
+		bf = bookField(*book)
+		res.Books = append(res.Books, bf)
+	}
+
+	// クライアントにレスポンスを返却
+	w.Header().Set("Content-Type", "application/json")
+	if err = json.NewEncoder(w).Encode(res); err != nil {
+		// TODO: エラーハンドリングをきちんとする
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+}
