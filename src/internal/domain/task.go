ad2ceb21b5d4aa37491f55eb76c6a294d7608ebf
Kaito Suzuki <snails8d@gmail.com>
2022-06-04 17:19:33 +0900

    add: task domain の作成

--- src/internal/domain/task.go
+++ src/internal/domain/task.go
@@ -10,15 +10,6 @@ type Task struct {
 	Content string
 }
 
-/**
- * domain層 model
- * modelにはそのmodelの構造体やビジネスロジックが置かれています。
- * taskがどういうものかを定義していきます。（今回はコンストラクタとセッターのみ）
- * 理想はこのファイルを見ればtaskがどういうmodelかわかること(この場合、titleが必須ということがわかる)
- */
-// 引数二つがわからない -> 型定義
-
-// NewTask taskのコンストラクタ
 func NewTask(title, content string) (*Task, error) {
 	if title == "" {
 		return nil, errors.New("titleを入力してください")
@@ -30,16 +21,4 @@ func NewTask(title, content string) (*Task, error) {
 	}
 
 	return task, nil
-}
-
-// Set task のセッター
-func (t *Task) Set(title, content string) error {
-	if title == "" {
-		return errors.New("titleを入力して下さい")
-	}
-
-	t.Title = title
-	t.Content = content
-
-	return nil
 }
