b1cd121579b1be6582512016ea48bb3596dc4004
Kaito Suzuki <snails8d@gmail.com>
2022-06-11 06:30:08 +0900

    add: task usecase の作成

--- src/internal/usecase/usecase.go
+++ src/internal/usecase/usecase.go
@@ -10,10 +10,22 @@ type databaseUsecase struct {
 	repository domain.Repository
 }
 
+type taskUsecase struct {
+	repository domain.TaskRepository
+}
+
 func NewUsecase(repository domain.Repository) databaseUsecase {
 	return databaseUsecase{repository: repository}
+}
+
+func NewTaskUsecase(repository domain.TaskRepository) taskUsecase {
+	return taskUsecase{repository: repository}
 }
 
 func (usecase *databaseUsecase) GetUsers(ctx context.Context, logger *middleware.Logger,) []domain.User {
 	return usecase.repository.GetUsers(ctx, logger)
 } 
+
+func (usecase *taskUsecase) GetTasks(ctx context.Context, logger *middleware.Logger) []domain.Task {
+	return usecase.repository.GetTasks(ctx, logger)
+}
