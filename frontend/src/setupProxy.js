106768645aec5f2a525f166e28768933918d5a8a
Kaito Suzuki <snails8d@gmail.com>
2022-06-26 13:43:17 +0900

    add: proxy の設定が死んでいたのでその修正

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,11 +1,11 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-//   app.use(proxy("/api/auth", { target: "http://localhost:8080/" }));
-    app.use(
-        proxy("/api", 
-        { 
-            target: process.env.REACT_APP_API_HOST,
-        }
-    ));
+module.exports = (app) => {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: process.env.REACT_APP_API_HOST,
+      changeOrigin: true,
+    })
+  );
 };
