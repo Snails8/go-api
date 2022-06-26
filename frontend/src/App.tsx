f587364fbc121748514f7a6563bc9eef54b8cede
Kaito Suzuki <snails8d@gmail.com>
2022-06-26 17:17:14 +0900

    add 404 ページの追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
-import { BrowserRouter, Link, Route, Router, Routes } from 'react-router-dom';
-import { Dashboard } from './pages/dashboard/Dashboard';
+import { RouterConfig } from './RouterConfig';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+      <RouterConfig />
   );
 }
 
--- frontend/src/RouterConfig.tsx
+++ frontend/src/RouterConfig.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SamplePage2 } from "./pages/pageB";
 import { Dashboard } from "./pages/dashboard/Dashboard";
 import { SamplePage1 } from "./pages/pageA";
+import { NotFound } from "./pages/notFound";
 
 
 export const RouterConfig:React.VFC =() => {
@@ -14,6 +15,7 @@ export const RouterConfig:React.VFC =() => {
         <Route index element={<Dashboard />} />
         <Route path="page1" element={<SamplePage1 />} />
         <Route path="page2" element={<SamplePage2 />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
     </>
