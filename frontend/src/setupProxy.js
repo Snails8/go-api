83eccf23a0fe1b9178fd70a5798067c3b3d9a929
Kaito Suzuki <snails8d@gmail.com>
2022-06-26 10:32:19 +0900

    add: proxy 設定追加

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,9 +3,9 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
 //   app.use(proxy("/api/auth", { target: "http://localhost:8080/" }));
     app.use(
-        proxy("/api/users", 
+        proxy("/api", 
         { 
-            target: "http://localhost:8081/" 
+            target: process.env.REACT_APP_API_HOST,
         }
     ));
 };
