04a3f01ddfd750d38b9713190fa24d99586db08e
Kaito Suzuki <snails8d@gmail.com>
2022-06-28 08:28:09 +0900

    add: sidebar のデザイン作成

---
+++ frontend/src/components/atoms/Sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styles from './Sidebar.module.css'
+
+export const Sidebar: React.FC = ({
+
+}) => { 
+    return (
+        <div className={`${styles.base}`}>
+            saaaaaaa
+        </div>
+    )
+
+}
