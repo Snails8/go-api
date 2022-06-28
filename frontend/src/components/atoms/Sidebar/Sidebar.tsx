a4bc905c3ffe5aab80225ea37e52b3edbea8435f
Kaito Suzuki <snails8d@gmail.com>
2022-06-29 07:06:58 +0900

    add: sidebar にrouting 機能追加

--- frontend/src/components/atoms/Sidebar/Sidebar.tsx
+++ frontend/src/components/atoms/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import styles from './Sidebar.module.css'
 
@@ -7,22 +8,31 @@ export const Sidebar: React.FC = ({
 }) => { 
     return (
         <div className={`${styles.base}`}>
-            <div className={ `${styles.sidebar_item}` }>
-                <div className={ `${styles.sidebar_text}`} >text</div>
-            </div>
-            <div className={ `${styles.sidebar_item}` }>
-                <div className={ `${styles.sidebar_text}`} >text</div>
-            </div>
-            <div className={ `${styles.sidebar_item}` }>
-                <div className={ `${styles.sidebar_text}`} >text</div>
-            </div>
-            <div className={ `${styles.sidebar_item}` }>
-                <div className={ `${styles.sidebar_text}`} >text</div>
-            </div>
-            <div className={ `${styles.sidebar_item}` }>
-                <div className={ `${styles.sidebar_text}`} >text</div>
-            </div>
+            <Link to="page1">
+                <div className={ `${styles.sidebar_item}` }>
+                    <div className={ `${styles.sidebar_text}`} >page1</div>
+                </div>
+            </Link>
+            <Link to="page2">
+                <div className={ `${styles.sidebar_item}` }>
+                    <div className={ `${styles.sidebar_text}`} >page2</div>
+                </div>
+            </Link>
+            <Link to="page3">
+                <div className={ `${styles.sidebar_item}` }>
+                    <div className={ `${styles.sidebar_text}`} >page3</div>
+                </div>
+            </Link>
+            <Link to="page4">
+                <div className={ `${styles.sidebar_item}` }>
+                    <div className={ `${styles.sidebar_text}`} >page4</div>
+                </div>
+            </Link>
+            <Link to="page1">
+                <div className={ `${styles.sidebar_item}` }>
+                    <div className={ `${styles.sidebar_text}`} >page1</div>
+                </div>
+            </Link>
         </div>
     )
-
 }
