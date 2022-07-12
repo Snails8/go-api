25bcfd6b69853e749b384f752d375e356507e349
Kaito Suzuki <snails8d@gmail.com>
2022-07-13 08:58:22 +0900

    add: table デザインの作成

--- frontend/src/pages/Users/Users.tsx
+++ frontend/src/pages/Users/Users.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { UsersTable } from "../../components/organisms/UsersTable/UsersTable";
 import { BasePage } from "../../components/templates/BasePage/BasePage";
 
 export const Users:React.FC = () => {
    
     return (
         <>
-            <BasePage>
-            aaaa
-            </BasePage>
+          <BasePage>
+            <UsersTable />  
+          </BasePage>
         </>
     )
 }
