02d30fecb9f6e3a02595e511521a3d08dfd93964
Kaito Suzuki <snails8d@gmail.com>
2022-07-21 08:57:39 +0900

    add: users をapiから取ってくる処理の追加

---
+++ frontend/src/hooks/useUsers.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+
+export interface User {
+    /**
+     * 
+     * @type {number}
+     * @memberof User
+     */
+    id: number;
+
+    /**
+     * 
+     * @type {string}
+     * @memberof User
+     */
+    name: string;
+}
+
+export const useUsers = () => {
+    const [users, setUsers] = useState<Array<User> | undefined>(undefined);
+
+    useEffect(() => {
+        fetch('http://localhost:7001/api/v1/users')
+            .then(res => res.json())
+            .then((users) => {
+                setUsers(users)
+            },
+            (error) => {
+                console.log(error);
+                const errData = {
+
+                }
+            }
+            )
+    }, [])
+
+    return {
+        users
+    }
+}
--- frontend/src/pages/Users/Users.tsx
+++ frontend/src/pages/Users/Users.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import { UsersTable } from "../../components/organisms/UsersTable/UsersTable";
 import { BasePage } from "../../components/templates/BasePage/BasePage";
 
+import {
+  useUsers
+} from '../../hooks/useUsers';
+
 export const Users:React.FC = () => {
-   
+  const users = useUsers();
+  
     return (
         <>
           <BasePage>
