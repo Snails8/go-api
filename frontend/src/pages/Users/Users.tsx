f2b61b92c8f7abf8d6623956b5d51f1392c09b12
Kaito Suzuki <snails8d@gmail.com>
2022-07-21 21:39:17 +0900

    add: 検証のため型定義を追加

--- frontend/src/pages/Users/Users.tsx
+++ frontend/src/pages/Users/Users.tsx
@@ -3,11 +3,17 @@ import { UsersTable } from "../../components/organisms/UsersTable/UsersTable";
 import { BasePage } from "../../components/templates/BasePage/BasePage";
 
 import {
+  User,
   useUsers
 } from '../../hooks';
 
 export const Users:React.FC = () => {
-  const users = useUsers();
+  // TODO::コンパイルを通すため一時的にany
+  // 解消方法はfetch の型がany になってしまうのでその修正
+  // const users = useUsers();
+  const users:any = useUsers();
+
+  console.log(users)
 
     return (
         <>
