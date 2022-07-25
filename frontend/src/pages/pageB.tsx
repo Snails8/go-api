63f3f81c5be37c350f316c5afb5756de6ce173d6
Kaito Suzuki <snails8d@gmail.com>
2022-07-25 10:04:40 +0900

    add: 別タブを開く実装の作成

--- frontend/src/pages/pageB.tsx
+++ frontend/src/pages/pageB.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
+import { BasePage } from "../components/templates/BasePage/BasePage";
 
 export const SamplePage2:React.FC =() => {
 
@@ -10,9 +11,12 @@ export const SamplePage2:React.FC =() => {
 
     return (
         <>
+        <BasePage>
             <h3>Sample Page 2</h3>
             <p>name={query1}</p>
             <p>type={query2}</p>
+            <button onClick={() => window.open("page3")}>別タブで開く</button>
+        </BasePage>
         </>
     )
 }
