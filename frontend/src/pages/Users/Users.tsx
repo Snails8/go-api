328be1617941f55f2f9000885359e9fbb8a5800f
Kaito Suzuki <snails8d@gmail.com>
2022-07-26 08:44:12 +0900

    add: API通信ができるように修正

--- frontend/src/pages/Users/Users.tsx
+++ frontend/src/pages/Users/Users.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { UsersTable } from "../../components/organisms/UsersTable/UsersTable";
 import { BasePage } from "../../components/templates/BasePage/BasePage";
 
+import { useEffect, useState } from 'react';
+
 import {
   User,
   useUsers
@@ -11,9 +13,22 @@ export const Users:React.FC = () => {
   // TODO::コンパイルを通すため一時的にany
   // 解消方法はfetch の型がany になってしまうのでその修正
   // const users = useUsers();
-  const users:any = useUsers();
-
-  console.log(users)
+  const [users, setUsers] = useState<User[]>([]);
+  
+  useEffect(() => {
+      fetch('http://localhost:7001/api/v1/users',{
+          method: 'GET'
+      })
+      .then(res => res.json())
+      .then((data) => {
+        setUsers(data.users)
+      },
+      (error) => {
+          console.log(error);
+          const errData = {
+          }
+      })
+  }, [])
 
     return (
         <>
