a46ab028e1ad0dfd109d4d63ce03a66f32fd6473
Kaito Suzuki <snails8d@gmail.com>
2022-07-26 14:50:23 +0900

    add: 子 → 親間でデータを同期するように実装

--- frontend/src/pages/Tabs/ChildTab.tsx
+++ frontend/src/pages/Tabs/ChildTab.tsx
@@ -3,6 +3,7 @@ import { BasePage } from "../../components/templates/BasePage/BasePage";
 
 export const ChildTabPage:React.FC =() => {
     const [message, setMessage] = useState(JSON.parse(localStorage.getItem('key') as string))
+    const [childString, setChildString] = useState('')
 
     useEffect(() => {
         window.addEventListener('storage', storageChange)
@@ -12,15 +13,33 @@ export const ChildTabPage:React.FC =() => {
         setMessage(JSON.parse(localStorage.getItem('key') as string))
     }
 
+
+    useEffect(() => {
+        // ここリロード必要
+        if (window.opener == null) {
+            return window.close()
+        } else {
+            window.opener.document.getElementById("childString").value = childString
+        }
+    },[childString])
+
+    const handleChange = (e:any) => {
+        setChildString(e.target.value)
+    }
     
     return (
         <>
         <BasePage>
             <h3>子タブ</h3>
-            <div>
-                <p>受け取ったメッセージ：「{message}」</p>
-            </div>
+            <h4>localStorageで親 → 子 を同期する</h4>
+            <p>受け取ったメッセージ：「{message}」</p>
             <button onClick={() => window.open("page3")}>別タブで開く</button>
+
+            <h4>window.openerで親tabに値を送信する</h4>
+            <label>text</label>
+            <input type="text" name="childString" defaultValue={childString}
+                onChange={(e) => setChildString(e.target.value)}
+            />
         </BasePage>
         </>
     )
--- frontend/src/pages/Tabs/ParentTab.tsx
+++ frontend/src/pages/Tabs/ParentTab.tsx
@@ -3,6 +3,7 @@ import { BasePage } from "../../components/templates/BasePage/BasePage";
 
 export const ParentTabPage:React.FC =() => {
     const [message, setMessage] = useState('message')
+    const [childString, setChildString] = useState('')
 
     useEffect(() => {
         localStorage.setItem('key', JSON.stringify(message));
@@ -12,18 +13,25 @@ export const ParentTabPage:React.FC =() => {
         setMessage(e.currentTarget.value)
         localStorage.setItem('key', JSON.stringify(message));
     }
-
-
+    
+    useEffect(() => {
+        console.log(childString)
+    }, [childString])
     return (
         <>
         <BasePage>
             <h3>親タブ</h3>
             <button onClick={() => window.open("child-tab")}>別タブで開く</button>
 
-            <br/>
+            <h4>localStorageを使用した親 → 子 の受け渡し方法</h4> <br/>
             <label>text</label>
             <input type="text" name="text" defaultValue={message}
                 onChange={handleChange}
+            />
+
+            <h4>window.opener を使用して子タブから値を受け取る</h4> <br/>
+            <label>text</label>
+            <input type="text" name="childString" id="childString" defaultValue={childString}
             />
         </BasePage>    
         </>
