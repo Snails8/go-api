8bcb914f74695019e9063af03c2862c4d0aef35f
Kaito Suzuki <snails8d@gmail.com>
2022-07-26 20:37:10 +0900

    add: users定義にemail を追加

--- backend/src/tools/seeds/local/seeds.sql
+++ backend/src/tools/seeds/local/seeds.sql
@@ -1,8 +1,8 @@
 DELETE FROM public.users;
 
-COPY public.users (id, name, created_at, updated_at) FROM stdin;
-1	管理者	2022-05-12	2022-05-12
-2	テスト	2022-05-12	2022-05-12
+COPY public.users (id, name, email, created_at, updated_at) FROM stdin;
+1	管理者	admin@test.com	2022-05-12	2022-05-12
+2	テスト	test@test.com	2022-05-12	2022-05-12
 \.
 
 SELECT pg_catalog.setval('public.users_id_seq', (SELECT MAX(id) FROM public.users), true);
