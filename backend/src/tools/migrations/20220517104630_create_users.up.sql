e32e67b09f4fc90b74204afdc77e40e372a34a69
Kaito Suzuki <snails8d@gmail.com>
2022-07-27 07:21:03 +0900

    add: upsertを使うためにemailにunique制約を追加

--- backend/src/tools/migrations/20220517104630_create_users.up.sql
+++ backend/src/tools/migrations/20220517104630_create_users.up.sql
@@ -5,5 +5,7 @@ CREATE TABLE IF NOT EXISTS users
     email varchar(256) NOT NULL,
     created_at timestamp with time zone NOT NULL,
     updated_at timestamp with time zone NOT NULL,
-    PRIMARY KEY (id)
+    
+    PRIMARY KEY (id),
+    UNIQUE (email)
 );
