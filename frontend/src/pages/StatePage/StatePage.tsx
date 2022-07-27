ec089973b12e44e76133dd28c98cb703701056ab
Kaito Suzuki <snails8d@gmail.com>
2022-07-27 21:02:19 +0900

    add: reload検知処理の作成

--- frontend/src/pages/StatePage/StatePage.tsx
+++ frontend/src/pages/StatePage/StatePage.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BasePage } from "../../components/templates/BasePage/BasePage";
 
 export const StatePage:React.FC =() => {
-
+    const [reloadText, setReloadText] = useState<string>("") 
+    
+    useEffect(() => {
+        setReloadText(localStorage.getItem('reload') as string)
+        
+        window.addEventListener('beforeunload', (e) => {
+            console.log("reloadりろーど時に実行される")
+            localStorage.setItem('reload', "reloadされたよ")
+        })
+    },[])
     return (
         <>
         <BasePage>
             <h3>状態保持の検証</h3>
 
             <h3>ブラウザのリロードを感知する</h3>
+            <span>{reloadText}</span>
         </BasePage>
         </>
     )
