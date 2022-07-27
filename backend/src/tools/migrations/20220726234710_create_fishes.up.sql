8157a9f898c12a6f7f80cb70d6d7b912aa76e2ef
Kaito Suzuki <snails8d@gmail.com>
2022-07-27 09:22:08 +0900

    add: 魚を扱うtable を追加

---
+++ backend/src/tools/migrations/20220726234710_create_fishes.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS fishes
+(
+    id serial,
+    name varchar(256) NOT NULL,
+    type_id INT NOT NULL, 
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone NOT NULL,
+
+    PRIMARY KEY (id),
+    UNIQUE (name)
+)
--- backend/src/tools/seeds/local/seeds.sql
+++ backend/src/tools/seeds/local/seeds.sql
@@ -5,4 +5,11 @@ COPY public.users (id, name, email, created_at, updated_at) FROM stdin;
 2	テスト	test@test.com	2022-05-12	2022-05-12
 \.
 
+COPY public.fishes (id, name, type_id, created_at, updated_at) FROM stdin;
+1	オイカワ	1	2022-05-12	2022-05-12
+2	カワムツ	1	2022-05-12	2022-05-12
+3	ドジョウ	1	2022-05-12	2022-05-12
+100	真鯛	2	2022-05-12	2022-05-12
+\.
+
 SELECT pg_catalog.setval('public.users_id_seq', (SELECT MAX(id) FROM public.users), true);
