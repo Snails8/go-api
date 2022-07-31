462e2642f1a2ddb94702df2c1460a53fa2ce6994
Kaito Suzuki <snails8d@gmail.com>
2022-08-01 08:51:12 +0900

    add: reload 感知機能の追加

--- frontend/src/pages/StatePage/StatePage.tsx
+++ frontend/src/pages/StatePage/StatePage.tsx
@@ -7,14 +7,16 @@ export const StatePage:React.FC = ({
     const [reloadText, setReloadText] = useState<string>("") 
     
     useEffect(() => {
-        setReloadText(localStorage.getItem('reload') as string)
-        
         window.addEventListener('beforeunload', (e) => {
-            console.log("reloadりろーど時に実行される")
-            localStorage.setItem('reload', "reloadされたよ")
-
-            //ここでstateを入れてもリセットされる
+            // console.log("reloadりろーど時に実行される")
+            localStorage.setItem('reload', "reloaded")
         })
+        
+        if (localStorage.getItem('reload') === "reloaded") {
+            setReloadText(localStorage.getItem('reload') as string)
+        }
+
+        localStorage.setItem('reload', "")
     },[])
 
     return (
@@ -22,8 +24,8 @@ export const StatePage:React.FC = ({
         <BasePage>
             <h3>状態保持の検証</h3>
 
-            <h3>ブラウザのリロードを感知する</h3>
-            <span>{reloadText}</span>
+            <h3>ブラウザのリロードを感知してリロード時のみ状態を保持する</h3>
+            <span>status:{reloadText}</span>
 
             <h3>遷移元のURLを取得する</h3>
             <span>{window.document.referrer}</span>
