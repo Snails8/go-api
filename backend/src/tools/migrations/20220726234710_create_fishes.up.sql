31f91deb34d762c8539a28cb28a899d7368e458e
Kaito Suzuki <snails8d@gmail.com>
2022-08-02 09:02:54 +0900

    add: 検証用のデータ,カラム追加

--- backend/src/tools/migrations/20220726234710_create_fishes.up.sql
+++ backend/src/tools/migrations/20220726234710_create_fishes.up.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS fishes
 (
     id serial,
     name varchar(256) NOT NULL,
-    type_id INT NOT NULL, 
+    classification text NOT NULL,
+    type_id INT NOT NULL,
     created_at timestamp with time zone NOT NULL,
     updated_at timestamp with time zone NOT NULL,
 
